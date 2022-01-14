aa74ed9a99bdc796e9d9713d5b8eedc219cd568d
Alfre <alfrermtmadrid@gmail.com>
2022-01-14 17:01:40 -0500

    Created component to show houses list

---
+++ src/components/HousesList.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+
+export default function HousesList({itemData}) {
+  return (
+    <ImageList sx={{ width: '100%', height: '100%', borderRadius: 2, marginTop: 0 }} cols={3} rowHeight={164}>
+      {/* <ImageListItem key="Subheader" cols={3}>
+        <ListSubheader component="div">Houses</ListSubheader>
+      </ImageListItem> */}
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.title}
+            subtitle={item.author}
+            actionIcon={
+              <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                aria-label={`info about ${item.title}`}
+              >
+                <InfoIcon />
+              </IconButton>
+            }
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
